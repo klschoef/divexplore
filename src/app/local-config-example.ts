b59a9d62439d6e2532b9af78b04aa99ff59e20d7
klschoef <git4klsmirana20>
2024-01-17 15:56:55 +0100

    added local config example

---
+++ src/app/local-config-example.ts
@@ -0,0 +1,13 @@
+export class LocalConfig {
+    public static config_CLIP_SERVER_HOST = 'localhost';
+    public static config_CLIP_SERVER_PORT = '8002';
+
+    public static config_NODE_SERVER_HOST = 'localhost';
+    public static config_NODE_SERVER_PORT = '8080';
+    
+    public static config_DATA_BASE_URL = 'http://localhost/diveXplore/';
+    public static config_DATA_BASE_URL_VIDEOS = 'http://localhost/videos/';
+
+    public static config_USER = 'DRESusername';
+    public static config_PASS = 'DRESpassword';
+}
